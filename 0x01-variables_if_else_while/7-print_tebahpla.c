df1113aee3fe05945baef740bd3f57fb30f145e1
GraceGichuki <gracegichuki2@gmail.com>
2022-09-09 15:15:44 -0700

    putchar

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,19 +1,21 @@
-/*print the alphabet in lowercase, followed by a new line.*/
-
+/*lowercase alphabets in reverse*/
 #include <stdio.h>
 
 /**
  * main-entry
  * Return: zero
  */
+
 int main(void)
 {
 	char ch;
 
-	for (ch = 'a'; ch <= 'z'; ch++)
+	for (ch = 'z'; ch >= 'a'; ch--)
 	{
 		putchar(ch);
 	}
+
 	putchar('\n');
+
 	return (0);
 }
