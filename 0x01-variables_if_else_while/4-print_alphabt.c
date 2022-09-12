611761fd48393ec1e9ea0ec8313b8fe5eb06995f
GraceGichuki <gracegichuki2@gmail.com>
2022-09-12 13:33:43 -0700

    if else statements

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+/*Print all the letters except q and e*/
+
+#include <stdio.h>
+
+/**
+ * main-entry
+ * Return:zero
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i != 'e' || i != 'q')
+			i++;
+		else
+			putchar (i);
+	)
+		putchar ('\n');
+
+	return (0);
+}
