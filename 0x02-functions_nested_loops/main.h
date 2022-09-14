3f169d8c631ce11cbe9be7287747c7d6244f9d45
GraceGichuki <gracegichuki2@gmail.com>
2022-09-14 14:02:26 -0700

    putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,19 @@
+/*functions*/
+#include <stdio.h>
+
 int test1(char *word)
 {
 	printf("%s\n", word);
 	return (0);
+}
+
+/*print alphabet in lowercase*/
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar (i);
+	putchar ('\n');
+	return;
 }
