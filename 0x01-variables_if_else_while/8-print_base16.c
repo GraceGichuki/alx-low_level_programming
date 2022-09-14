798415c70d89ea6ba2d693062f6f918d577d5cfb
GraceGichuki <gracegichuki2@gmail.com>
2022-09-14 02:53:45 -0700

    putchar

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+/*print all numbers of base 16 in lowercase*/
+#include <stdio.h>
+
+/**
+ * main-entry point
+ * Return: zero
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '1'; i <= '9'; i++)
+		putchar(i);
+	for (i = 'a'; i <= 'e'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
