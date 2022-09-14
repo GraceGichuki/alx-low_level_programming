d9bc340d05cc5c793727221b454a9ef70a9d432a
GraceGichuki <gracegichuki2@gmail.com>
2022-09-14 06:11:40 -0700

    putchar

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+/* prints all possible combinations of two two-digit numbers*/
+#include <stdio.h>
+/**
+ * main-entry
+ * Return: zero
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+
+	for (i = '0'; i <= '9'; i++)
+		for (j = '0'; j <= '9' j++)
+			for (k ='0'; k <= '9'; k++)
+				for (l ='0'; l <='9';)
+					if (i == k && j == l)
+						l++;
+					else 
+					{
+						putchar (i);
+						putchar (j);
+						putchar (' ');
+						putchar (k);
+						putchar (l);
+						putchar (',');
+						putchar (' ');
+						k++;
+					}
+	putchar ('\n);
+	return (0);
+}
