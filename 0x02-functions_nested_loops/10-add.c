1bea6482f348f1fdd130ef4628792959190b10e2
GraceGichuki <gracegichuki2@gmail.com>
2022-09-15 02:13:16 -0700

    functions

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *add - perform addition of two integers
+ *@a: intger one
+ *@b: integer two
+ *
+ *Return: result if success
+ */
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+
+	return (result);
+}
