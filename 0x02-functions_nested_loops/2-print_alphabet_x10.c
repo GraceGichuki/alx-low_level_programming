24ce78cd3db829ba501d4df4f2fddcbc97346a01
GraceGichuki <gracegichuki2@gmail.com>
2022-09-15 00:19:45 -0700

    functions

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,13 +7,15 @@
 void print_alphabet_x10(void)
 {
 	int i;
-
-	i = print_alphabet();
-
-	for (j = 0; j < 10; j++)
+	int j
+		
+	for (i = 0; i < 10; i++)
 	{
-		_putchar(i);
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
 
 	}
-	_putchar('/n');
 }
