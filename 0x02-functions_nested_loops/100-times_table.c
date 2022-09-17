75488884e9996bbdd6036f8fb0256a693d513393
GraceGichuki <gracegichuki2@gmail.com>
2022-09-17 10:35:19 -0700

    functions

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints the n times table, starting with 0
+ * @n: table to be printed
+ *
+ * Return: zero
+ */
+
+void print_times_table(int n)
+{
+	int i, j, m;
+
+	if (n >= 0 && < 16)
+	{
+		for (i = 0; i < n + 1; i++)
+		{
+			for (j = 0; j < n + 1; j++)
+			{
+				m = i * j;
+				if (m > 99)
+				{
+					_putchar(m / 100 + '0');
+					_putchar((m / 10) % 10 + '0');
+				}
+				else if (m > 9)
+					_putchar(m / 10 + '0');
+				else (j != 0)
+					_putchar(' ');
+				_putchar(m % 10 + '0');
+			}
+			_putchar('\n');
+		}
+	}
+}
