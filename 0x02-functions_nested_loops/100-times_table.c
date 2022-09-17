0a000a77479e4e9cb00a5be78503e624ff44a28b
GraceGichuki <gracegichuki2@gmail.com>
2022-09-17 10:54:36 -0700

    functions

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -28,7 +28,7 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar(m / 10 + '0');
 				}
-				else if (j != 0)
+				else if (m < 10 && j != 0)
 				{
 					_putchar(' ');
 					_putchar(' ');
