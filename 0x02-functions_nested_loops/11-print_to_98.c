4ee78b5e755758dc9ba1a7eb3e99679a746adaed
GraceGichuki <gracegichuki2@gmail.com>
2022-09-17 04:36:42 -0700

    functions

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -17,7 +17,7 @@ void print_to_98(int n)
 		for (i = n; i > 97; i--)
 		{
 			printf("%d", i);
-			if (n != 98)
+			if (i != 98)
 			{
 				putchar(',');
 				putchar(' ');
@@ -29,7 +29,7 @@ void print_to_98(int n)
 		for (i = n; i < 99; i++)
 		{
 			printf("%d", i);
-			if (n != 98)
+			if (i != 98)
 			{
 				putchar(',');
 				putchar(' ');
