2e2f7944cbca2f32ef37e62d350cf379c328b57a
GraceGichuki <gracegichuki2@gmail.com>
2022-09-19 03:38:24 -0700

    functions

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - entry
+ * Return: zero
+ */
+
+int main(void)
+{
+	int num1, num2, num3, sum, n;
+
+	num1 = 0;
+	num2 = 1;
+	sum = 0;
+
+	for (n =0; n < 4000000; n++)
+	{
+		num3 = num1 + num2;
+		if (num3 % 2 == 0)
+			sum = sum + num3;
+		num1 = num2;
+		num2 = num3;
+	}
+	printf("%lld\n");
+	return (0);
+}
