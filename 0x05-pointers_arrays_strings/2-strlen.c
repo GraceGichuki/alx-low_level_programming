7608e61c7fe60e22302d7e41a4b3a18e245b560d
GraceGichuki <gracegichuki2@gmail.com>
2022-09-19 13:19:11 -0700

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen - lenth of a string
+ * @s: pointer to a string
+ *
+ * Return: length of a string
+ */
+int _strlen(char *s)
+{
+	char d;
+
+	len = strlen(d);
+	return (len);
+}
