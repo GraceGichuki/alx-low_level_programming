c99bebd71528ca6011a1186d36a8844147dc5100
GraceGichuki <gracegichuki2@gmail.com>
2022-09-19 02:12:54 -0700

    functions

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,16 +7,16 @@
 
 int main(void)
 {
-	int num1, num2, num3 n;
+	int num1, num2, num3, n;
 
 	num1 = 0;
 	num2 = 1;
 
-	for (n = 0; n < 50; n++)
+	for (n = 0; n < 99; n++)
 	{
 		num3 = num1 + num2;
 		printf("$d", num3);
-		if (n != 49)
+		if (n != 98)
 			printf(", ");
 		num1 = num2;
 		num2 = num3;
