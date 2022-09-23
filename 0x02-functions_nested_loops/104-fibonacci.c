5424d5f26fc21e5b941d7596fe65333bb3f17188
GraceGichuki <gracegichuki2@gmail.com>
2022-09-23 05:33:35 -0700

    functions

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -13,11 +13,11 @@ int main(void)
 	num1 = 0;
 	num2 = 1;
 
-	for (n = 0; n < 99; n++)
+	for (n = 0; n < 98; n++)
 	{
 		num3 = num1 + num2;
 		printf("%u", num3);
-		if (n != 98)
+		if (n != 97)
 			printf(", ");
 		num1 = num2;
 		num2 = num3;
