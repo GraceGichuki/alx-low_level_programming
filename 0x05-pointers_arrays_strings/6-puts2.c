5586baa889587e53d864e9c8c7d9b192bc019335
GraceGichuki <gracegichuki2@gmail.com>
2022-10-21 01:45:44 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * puts2 - print all characters in a string in order
+ * str: charater array
+ *
+ * Return: nothing
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; *str; i++)
+		_putchar(*str++);
+	_putchar('\n');
+}
