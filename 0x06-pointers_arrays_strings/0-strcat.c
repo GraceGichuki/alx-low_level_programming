4ee763ff8e5099be53e913956f65b30bd01a24e7
GraceGichuki <gracegichuki2@gmail.com>
2022-10-21 03:07:16 -0700

    more pointers

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * *_strcat - conxatenates two strings
+ * @dest: string  to be added to
+ * @src: string to be added
+ *
+ * Return: pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * *_strncat  -concatenates two strings
+ * @dest: string to be added to
+ * @src: string to be added
+ * @n:
+ *
+ * Return: pointer to the resulting string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	return (dest);
+}
