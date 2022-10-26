4ca76c6a37503a3acba2dd4bc3fac1716d567ec8
GraceGichuki <gracegichuki2@gmail.com>
2022-10-26 12:14:54 -0700

    pointers

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,11 +10,8 @@ char *_strcpy(char *dest, char *src)
 {
 	int i;
 
-	for (i = 0; ; i++)
-	{
-		*dest++ = *src++;
-		if (*src == 0)
-			break;
-	}
+	for (i = 0; src[i] ; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
 	return (dest);
 }
