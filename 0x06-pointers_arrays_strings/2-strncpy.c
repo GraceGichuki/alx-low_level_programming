674f2c96d81a534aa709e0d5be476e1016057db1
GraceGichuki <gracegichuki2@gmail.com>
2022-10-27 05:11:07 -0700

    more pointers

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -3,7 +3,7 @@
  * *_strncpy - copies a string
  * @dest: pointer to where the string will be copied to
  * @src: pointer to the string that is to copied
- * @n: iteration
+ * @n: number of byts to be copied
  *
  * Return: dest
  */
@@ -13,7 +13,7 @@ char *_strncpy(char *dest, char *src, int n)
 
 	for (i = 0; i < n && src[i]; i++)
 		dest[i] = src[i];
-	if ( i < n - 1)
+	if (i < n - 2)
 		dest[i] = '\0';
 	return (dest);
 }
