420cf51b84ab277b3bec73f4331c3515c11896b4
GraceGichuki <gracegichuki2@gmail.com>
2022-10-27 04:23:41 -0700

    more pointers

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -3,11 +3,19 @@
  * *_strncat  -concatenates two strings
  * @dest: string to be added to
  * @src: string to be added
- * @n:
+ * @n: number of bytes from src
  *
  * Return: pointer to the resulting string dest
  */
 char *_strncat(char *dest, char *src, int n)
 {
+	int i, j;
+
+	for (i = 0; dest[i]; i++)
+		;
+	for (j = 0; j < n; j++, i++)
+		dest[i] = src[j];
+	if (j < n - 1;)
+		dest[i] = '\0';
 	return (dest);
 }
