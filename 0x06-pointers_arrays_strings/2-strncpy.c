0e55f60587c13fba7e2d50ba3a8400f00c43112d
GraceGichuki <gracegichuki2@gmail.com>
2022-10-27 05:01:27 -0700

    more pointers

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,8 +9,8 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	for (n = 0; src[n]; n++)
-		dest[n] = src[n];
-	dest[n] = '\0';
-	return (dest);
+	for (n = 0; dest[n]; n++)
+		src[n] = dest[n];
+	src[n] = '\0';
+	return (src);
 }
