cee9e8a264c7490461ed544cc30412abf118b248
GraceGichuki <gracegichuki2@gmail.com>
2023-02-21 03:42:09 -0800

    task 2 update

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,15 @@
 #include "main.h"
 /**
  * print_alphabet_x10 - print alphabet ten times
- *
- * Return: nothing
  */
 void print_alphabet_x10(void)
 {
-	int i;
-	int j;
+	int i, j;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 'a'; j <= 'z'; j++)
-		{
 			_putchar(j);
-		}
 		_putchar('\n');
-
 	}
 }
