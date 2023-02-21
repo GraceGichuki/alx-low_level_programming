d8a81023aaaa22f9c4a5c8cf824671d22369fc20
GraceGichuki <gracegichuki2@gmail.com>
2023-02-21 05:34:56 -0800

    task 9 update

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,18 +8,24 @@
 
 void times_table(void)
 {
-	int i, j, n;
+	int i, j;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			n = i * j;
-			if (n > 9)
-				_putchar(n / 10 + '0');
-			else if (j != 0)
+			if (j == 0)
+				_putchar((i * j) + '0');
+			else if ((i * j) < 10)
+			{
 				_putchar(' ');
-			_putchar(n % 10 + '0');
+				_putchar((i * j) + '0');
+			}
+			else
+			{
+				_putchar((i * j) / 10 + '0');
+				_putchar((i * j) % 10 + '0');
+			}
 			if (j != 9)
 			{
 				_putchar(',');
