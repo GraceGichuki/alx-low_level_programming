95e4411a6574e4e973bf7575b224d8c06eebbd13
GraceGichuki <gracegichuki2@gmail.com>
2023-03-01 01:10:55 -0800

    task 6 update

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -11,9 +11,12 @@ void print_line(int n)
 {
 	int i;
 
-	for (i = 1; i < n + 1; i++)
+	if (n > 0)
 	{
-		_putchar('_');
+		for (i = 0; i < n; i++)
+			_putchar('_');
+		_putchar('\n');
 	}
-	_putchar('\n');
+	else
+		_putchar(10);
 }
